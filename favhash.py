880c79842c80c4699d8ba838be0c5107b5a66b98
Sachin Acharya <iiit.sachin@gmail.com>
2014-07-22 19:28:38 +0530

    Update favhash.py

--- favhash.py
+++ favhash.py
@@ -1,4 +1,3 @@
-
 import twitter
 
 CONSUMER_KEY = ""
@@ -10,26 +9,34 @@ auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUM
 twitter_api = twitter.Twitter(auth=auth)
 print twitter_api
 
-
+hasharray = []
 statuses = twitter_api.statuses.user_timeline(screen_name = '@xyz', max_count= 30, include_entities=1 )
-
 for tweet in statuses:
 	try:
 		for hashtag in tweet['entities']['hashtags']:
 			print hashtag['text']
 			hashtagtext = '#' +hashtag['text'] 
-			count = 5
-			print str(hashtagtext)
-			search_results = twitter_api.search.tweets(q=str(hashtagtext), count=count)
-			stat = search_results['statuses']
-			for tweets in stat:
-				try:
-					print tweets['text'], '\n'
-					result = twitter_api.favorites.create(_id=tweets['id'])
-					print("favorited: %s" % (result['text'].encode("utf-8")))
-				except twitter.api.TwitterHTTPError as e:
-					print("error: %s" % (str(e)))
-
+			hasharray.append(hashtagtext)
 	except twitter.api.TwitterHTTPError as e:
 		print("error: %s" % (str(e)))
+count = 5
+print hasharray
+newarray = []
+
+for i in hasharray:
+	if i not in newarray:
+		newarray.append(i)
+print newarray
+
+for k in newarray:
+	print str(i)
+	search_results = twitter_api.search.tweets(q=str(i), count=count)
+	stat = search_results['statuses']
+	for tweets in stat:
+		try:
+			print tweets['text'], '\n'
+			result = twitter_api.favorites.create(_id=tweets['id'])
+			print("favorited: %s" % (result['text'].encode("utf-8")))
+		except twitter.api.TwitterHTTPError as e:
+			print("error: %s" % (str(e)))
 
