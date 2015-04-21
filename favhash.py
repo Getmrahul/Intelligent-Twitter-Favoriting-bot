b52e59bd5befbad4196c74b13033ad1d01a0fa33
Rahul M <Getmrahul@users.noreply.github.com>
2015-04-21 12:00:39 +0530

    Update favhash.py

--- favhash.py
+++ favhash.py
@@ -11,23 +11,9 @@ print twitter_api
 
 #Add hastag in the below array
 hasharray = ['#tagsdock','#instagram','#hashtags','#vine']
-#statuses = twitter_api.statuses.user_timeline(screen_name = '@xyz', max_count= 30, include_entities=1 )
-#for tweet in statuses:
-#	try:
-#		for hashtag in tweet['entities']['hashtags']:
-#			print hashtag['text']
-#			hashtagtext = '#' +hashtag['text'] 
-#			hasharray.append(hashtagtext)
-#	except twitter.api.TwitterHTTPError as e:
-#		print("error: %s" % (str(e)))
+
 count = 5
 print hasharray
-#newarray = []
-
-#for i in hasharray:
-#	if i not in newarray:
-#		newarray.append(i)
-#print newarray
 
 for k in hasharray:
 	print k
